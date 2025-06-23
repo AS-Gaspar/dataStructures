736f8a9a783afe1b61ebbb149afe66d756d5c92e
AS-Gaspar <airtontunni@gmail.com>
2025-06-23 16:26:04 -0300

    arrays

---
+++ arrays.js
@@ -0,0 +1,9 @@
+let object = {
+    teste1: 111,
+    teste2: 222,
+    teste3: 333
+}
+
+console.log(Object.entries(object))
+console.log(Object.values(object))
+console.log(object.hasOwnProperty("teste1"))
