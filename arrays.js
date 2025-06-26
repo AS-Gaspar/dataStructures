c20199b375c65ca04b7e9f3214393d9694b2b7fa
gaspar <airtontunni@gmail.com>
2025-06-25 21:50:39 -0300

    first problem solving

--- arrays.js
+++ arrays.js
@@ -1,9 +1,18 @@
-let object = {
-    teste1: 111,
-    teste2: 222,
-    teste3: 333
+const arr1 = [4, 16, 36, 81]
+const arr2 = [9, 4, 2, 6]
+
+function compareArrs(arr1, arr2) {
+    if (arr1.length !== arr2.length) return false
+    const obj = {}
+    for (let index of arr1) {
+        obj[index] = Math.sqrt(index) 
+    }
+
+    for (let sqrt of arr2) {
+        if (!obj.hasOwnProperty(sqrt*sqrt)) return false
+    }
+
+    return true
 }
 
-console.log(Object.entries(object))
-console.log(Object.values(object))
-console.log(object.hasOwnProperty("teste1"))
+console.log(compareArrs(arr1, arr2))
--- node_c9529b59748ea.tmp.js
+++
@@ -1,9 +0,0 @@
-function addPreviousNumbers(n) {
-    let result = 0
-    for (let i = n; i >= 1; i++) {
-       result += i 
-    }
-    return result
-}
-
-console.log(addPreviousNumbers(6))
