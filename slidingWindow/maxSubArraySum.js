a39e247f1c9e718cc163d04a3bb5de7092e76b44
gaspar <airtontunni@gmail.com>
2025-07-09 23:47:57 -0300

    sliding window exercise done

--- slidingWindow/maxSubArraySum.js
+++ slidingWindow/maxSubArraySum.js
@@ -1,20 +1,29 @@
-const arr = [1, 4, 2, 5, 8, 10, 2, 1, 8, 5, 2];
-const num = 2;
+const arr = [1, 4, 2, 5, 8, 10, 2, 1, 8, 5, 2, 20]
+const num = 2
 
 function maxSubArraySum(arr, num) {
+  if (arr.length === 0) return null
   if (arr.length < num)
     return arr.reduce(
       (accumulator, currentValue) => accumulator + currentValue,
       0
     )
-    let sum = 0
-  for (let i = 0; i < arr.length; i++) {
-    if (i < num - 1) {
-        sum += arr[i]
-    } else {
-        
-    }
+
+  let max = 0;
+  let j = 0;
+
+  while (j < num) {
+    max += arr[j]
+    j++
   }
+
+  let tempSum = max
+
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i]
+    max = Math.max(max, tempSum)
+  }
+  return max
 }
 
-console.log();
+console.log(maxSubArraySum(arr, num))
